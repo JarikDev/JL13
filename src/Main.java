d3538aef8a3de781cce080aeeba886dbd5ef3149
ivan4000 <sxln1@mail.ru>
2018-10-02 16:36:30 +0300

    LJ13

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    int $;
+    int _asd;
+    int asdfasd;
+    int sdfasdfasdfadfasdfasdfgasdfasdgasdgasdgasdgasgdasdgasgdasdgasdfasdf;
+    int иояПеременная;
+    int _123123;
+    int $123;
+    int camelCase;
+    int myVariableVeryLongName;
+
+    void myMethodWithVeryLongNmae(){}
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    private String myName;
+    private boolean blond;
+
+    public boolean isBlond() {
+        return blond;
+    }
+
+    public void setBlond(boolean blond) {
+        this.blond = blond;
+    }
+
+    public String getMyName() {
+        return myName ;
+    }
+
+    public void setMyName(String name) {
+        this.myName = name;
+    }
+}
